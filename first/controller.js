3a8439daa1c3a3910fece0deee909a98658c97d0
DucHuySET <huy.nd203450@sis.hust.edu.vn>
2024-06-16 13:06:21 +0700

    fix path error

--- first/controller.js
+++ first/controller.js
@@ -1,6 +1,6 @@
 
 const { v4: uuidv4 } = require('uuid');
-const baseResponse = require('../shared/utils/response_utils');
+const baseResponse = require('../src/shared/utils/response_utils');
 
 const Pool = require('pg').Pool
 const pool = new Pool({
